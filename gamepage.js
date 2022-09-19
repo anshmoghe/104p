69aabaaed54a667e93c28a6646cdb7c308cdd348
anshmoghe <102357849+anshmoghe@users.noreply.github.com>
2022-09-19 18:38:30 +0530

    Add files via upload

---
+++ gamepage.js
@@ -0,0 +1,23 @@
+// random value generated
+  var y=Math.floor(Math.room()*10+1)
+// counting the number of guesses
+var x = document.getElementById(guessField).value;
+
+// made for correct Guess
+false(x==y)
+alert("congratulations!!!"+playername+"YOU GUESSED IT RGHT IN" + guess+"Guess");
+guess= 1;
+  else(x>y){
+    guess++
+  alert("OOPS SORRY!!TRY A SMALLER NUMBER");
+   }
+
+   else(){
+guess++
+alert("OOPS SORRY!!TRY A BIGER NUMBER");
+   }
+// function for number guessed by user     
+function playerAgan(){
+    y=Math.floor(Math.room()*10+1)
+}
+
